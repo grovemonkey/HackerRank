4e1d47661f81bbd4924a43fbc0448e25fde7880d
Sam Thomas <saleswithsam@gmail.com>
2020-10-18 12:29:52 -0600

    Tested another HR project on this

--- wordorder.py
+++ wordorder.py
@@ -2,15 +2,13 @@ from collections import Counter
 y = input()
 lines = []
 while True:
-    try: 
+    try:
         line = input()
     except EOFError:
         break
-    
+
     lines.append(line)
 
 counts = Counter(lines)
 print(len(counts))
-print(" ".join([str(v) for k,v in counts.items()]))
-
-
+print(" ".join([str(v) for k, v in counts.items()]))
